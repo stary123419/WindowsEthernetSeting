1c07974bbb592fc7bad809a66a4c0689534b921b
stary123419 <stary123419@gmail.com>
2025-04-06 01:49:18 +0300

    Update Ethernet setings.bat

--- Ethernet setings.bat
+++ Ethernet setings.bat
@@ -1,3 +1,2 @@
-netsh interface ip set address "Ethernet" static 80.64.30.13 255.255.255.0 80.64.30.1
-netsh interface ip dns "Ethernet" static 8.8.8.8 primary
-netsh interface ip dns "Ethernet" static 8.4.4.8 index=2
+netsh interface ip set address name="Ethernet" source=static 80.64.30.13 255.255.255.0 gateway=80.64.30.1
+netsh interface ip set dns name="Ethernet" static 8.8.8.8
