3fbe352b6188c868e0072aae723a760654811729
stary123419 <stary123419@gmail.com>
2025-04-07 19:22:47 +0300

    commit

--- Ethernet_settings.ps1
+++ Ethernet_settings.ps1
@@ -1,14 +1,17 @@
 $ip = (Get-Content -Path .\IPadress.txt)[0]
 $gateway = (Get-Content -Path .\IPadress.txt)[1]
-netsh interface ip set address name="Ethernet" source=static $ip 255.255.255.0 gateway=$gateway
-netsh interface ip set dns name="Ethernet" static 8.8.8.8
-netsh interface ip add dns name="Ethernet" address="8.8.4.4" index=2  
+$mask = (Get-Content -Path .\IPadress.txt)[2]
+$dns = (Get-Content -Path .\IPadress.txt)[3]
+$dns2 = (Get-Content -Path .\IPadress.txt)[4]
+netsh interface ip set address name="Ethernet" source=static $ip $mask gateway=$gateway
+netsh interface ip set dns name="Ethernet" static $dns
+netsh interface ip add dns name="Ethernet" address=$dns2 index=2  
 Start-Sleep -Seconds 15
 if ( -not (Test-Connection microsoft.com -Quiet ))
 {
     netsh interface ip set address "Ethernet" dhcp
     netsh interface ip set dns name="Ethernet" dhcp
-    netsh interface ip set address name="Ethernet 2" source=static $ip 255.255.255.0 gateway=$gateway
-    netsh interface ip set dns name="Ethernet 2" static 8.8.8.8
-    netsh interface ip add dns name="Ethernet 2" address="8.8.4.4" index=2    
+    netsh interface ip set address name="Ethernet 2" source=static $ip $mask gateway=$gateway
+    netsh interface ip set dns name="Ethernet 2" static $dns
+    netsh interface ip add dns name="Ethernet 2" address=$dns2 index=2    
 }
