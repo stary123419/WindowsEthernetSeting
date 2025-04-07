d685fda5c888d71d6c48212001d257b9f7927f99
stary123419 <stary123419@gmail.com>
2025-04-07 15:17:37 +0300

    commit

--- 2. Удаление из автозагрузки.ps1
+++ 2. Удаление из автозагрузки.ps1
@@ -1,3 +1,4 @@
+chcp 1251
 Write-Host "Удаление Ethernet_settings.bat из автозагрузки "
 Remove-Item "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Ethernet_settings.bat"
 
