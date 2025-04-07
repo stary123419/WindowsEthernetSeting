aeafaead7aa7ab84147bfc254d1af847062ab106
stary123419 <stary123419@gmail.com>
2025-04-07 15:46:35 +0300

    commit

--- Ethernet_settings.bat
+++ Ethernet_settings.bat
@@ -1,4 +1,14 @@
-netsh interface ip set address "Ethernet" dhcp
-netsh interface ip set address name="Ethernet 2" source=static 80.64.30.13 255.255.255.0 gateway=80.64.30.1
-netsh interface ip set address "Ethernet 2" dhcpnetsh interface ip set dns name="Ethernet" static 8.8.8.8
-netsh interface ip add dns name="Ethernet 2" address="8.8.4.4" index=2
+if ( Test-Connection 8.8.8.8 )
+{
+    netsh interface ip set address "Ethernet" dhcp
+    netsh interface ip set address name="Ethernet 2" source=static 80.64.30.13 255.255.255.0 gateway=80.64.30.1
+    netsh interface ip set dns name="Ethernet 2" static 8.8.8.8
+    netsh interface ip add dns name="Ethernet 2" address="8.8.4.4" index=2
+}
+else
+{
+    netsh interface ip set address "Ethernet 2" dhcp
+    netsh interface ip set address name="Ethernet" source=static 80.64.30.13 255.255.255.0 gateway=80.64.30.1
+    netsh interface ip set dns name="Ethernet" static 8.8.8.8
+    netsh interface ip add dns name="Ethernet" address="8.8.4.4" index=2
+}
