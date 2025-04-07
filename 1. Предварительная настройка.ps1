ff13761f464818272fe65666891dd78002a1928e
stary123419 <stary123419@gmail.com>
2025-04-07 10:52:08 +0300

    commit

--- 1. Предварительная настройка.ps1
+++ 1. Предварительная настройка.ps1
@@ -1,6 +1,5 @@
-Remove-Item 'C:\Users\Administrator\Downloads\111.zip'
 Copy-Item "C:\Users\Administrator\Downloads\111\WindowsEthernetSeting-main\Ethernet setings.bat" -Destination "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device' -Name DevicePasswordLessBuildVersion -Value 2
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device' -Name DevicePasswordLessBuildVersion -Value 0
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" `
                  -Name "UserAuthentication" `
--- 2. Удаление из автозагрузки.bat
+++ 2. Удаление из автозагрузки.bat
@@ -1,1 +1,2 @@
 del /f /s /q "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Ethernet setings.bat"
+Remove-Item 'C:\Users\Administrator\Downloads\111.zip'
