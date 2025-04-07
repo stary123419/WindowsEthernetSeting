51421d1513980eb8bc645623e173b20cb5f18f88
stary123419 <stary123419@gmail.com>
2025-04-07 09:50:01 +0300

    commit

--- 1. Предварительная настройка.ps1
+++ 1. Предварительная настройка.ps1
@@ -1,6 +1,5 @@
-Expand-Archive -Path 'C:\Users\Administrator\Downloads\111.zip' -DestinationPath 'C:\Users\Administrator\Downloads'
 del /f /s /q 'C:\Users\Administrator\Downloads\111.zip'
-Copy-Item -Path "C:\Users\Administrator\Downloads\WindowsEthernetSeting-main\Ethernet setings.bat" -Destination "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+Copy-Item -Path "C:\Users\Administrator\Downloads\WindowsEthernetSeting-main\111\Ethernet setings.bat" -Destination "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device' -Name DevicePasswordLessBuildVersion -Value 2
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" `
