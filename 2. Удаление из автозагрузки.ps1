5736801a7f609c7a0035f9492b74e52e69490d9b
stary123419 <stary123419@gmail.com>
2025-04-08 09:12:55 +0300

    commit

--- 2. Удаление из автозагрузки.ps1
+++ 2. Удаление из автозагрузки.ps1
@@ -1,7 +1,6 @@
 chcp 1251
 Write-Host "Удаление Ethernet_settings.exe из автозагрузки "
 Remove-Item "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Ethernet_settings.exe"
-Remove-Item "C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\IPadress.txt"
 
 Write-Host "Удаление архива 111.zip"
 Remove-Item 'C:\Users\Administrator\Downloads\111.zip'
